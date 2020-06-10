07dde5d3821d37fe216256b4932cc3f3972f8f5b
Tejas Navghane <TejasNavghane@djalexander.in>
2020-06-10 18:47:15 +0530

    [ADD] - divide functionality and updated test cases

---
+++ src/Exceptions/DivideByZeroException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use Exception;
+
+/**
+ * Class DivideByZeroException
+ * @package App\Exceptions
+ */
+class DivideByZeroException extends Exception
+{
+
+}
